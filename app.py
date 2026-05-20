25b70c54b2ca220586f8f7c6d7815e752559d57a
ivaylominkov-cloud <ivaylo.minkov@gmail.com>
2026-05-20 17:21:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import requests
-import os
 
-print("START ✅")
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = "YOUR_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.post(
+requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
         "text": "Hello ✅"
-    },
-    timeout=10
+    }
 )
 
-print("Telegram status:", response.status_code)
-print("END ✅")
+print("Message sent ✅")
+``
