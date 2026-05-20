dfc1f04a64a308d75a39de8abaffc3d023ac2042
ivaylominkov-cloud <ivaylo.minkov@gmail.com>
2026-05-20 17:11:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 import os
-import requests
 
-print("BOT STARTED ✅")
+print("✅ SCRIPT STARTED")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 print("Token exists:", BOT_TOKEN is not None)
 print("Chat ID exists:", CHAT_ID is not None)
 
-def send_test():
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    response = requests.post(
-        url,
-        data={"chat_id": CHAT_ID, "text": "✅ Bot is working!"},
-        timeout=10
-    )
-    print("Telegram response:", response.status_code)
-
-send_test()
-
-print("BOT FINISHED ✅")
+print("✅ SCRIPT FINISHED")
